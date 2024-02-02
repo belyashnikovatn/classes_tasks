89b19d2a42ea583ac103e7c3b2289c40e53ecdbe
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-02-02 12:06:04 +0500

    extened classes

--- Employee.py
+++ Employee.py
@@ -8,8 +8,12 @@ class Employee:
         self.second_name = second_name_value
         self.gender = gender_value
         self.remaining_vacation_days = Employee.vacation_days
+        self._employee_id = self.__generate_employee_id()
         #   очень важно EMPLOYEE.vacation_days то есть
         #   мы обращаемся к атрибуту КЛАССА
+
+    def __generate_employee_id(self):
+        return hash(self.first_name + self.second_name + self.gender)
 
     def consume_vacation(self, vacation_days):
         self.remaining_vacation_days -= vacation_days
@@ -19,10 +23,22 @@ class Employee:
 
 
 class FullTimeEmployee(Employee):
+
     def get_unpaid_vacation(self, start_date, vacation_days):
         return (f'Начало неоплачиваемого отпуска: '
                 f'{start_date}, '
                 f'продолжительность: {vacation_days} дней.')
+
+    def __init__(self, first_name_value,
+                 second_name_value, gender_value, salary_value) -> None:
+        self.__salary = salary_value
+        super().__init__(first_name_value, second_name_value, gender_value)
+
+    def __get_vacation_salary(self):
+        return self.__salary * 0.8
+
+    def get_salary_vacation(self):
+        return self.__get_vacation_salary()
 
 
 class PartTimeEmployee(Employee):
@@ -33,6 +49,15 @@ class PartTimeEmployee(Employee):
         super().__init__(first_name_value, second_name_value, gender_value)
         self.remaining_vacation_days = PartTimeEmployee.vacation_days
 
+
+full_time_employee = FullTimeEmployee('Иван', 'Иванов', 'м', 100)
+print(full_time_employee._employee_id)
+print(full_time_employee.get_unpaid_vacation('2023-07-01', 5))
+print(full_time_employee.get_salary_vacation())
+part_time_employee = PartTimeEmployee('Анна', 'Петрова', 'ж')
+part_time_employee.consume_vacation(5)
+print(part_time_employee._employee_id)
+print(part_time_employee.get_vacation_details())
 
 # full_time_employee = FullTimeEmployee('Роберт', 'Крузо', 'м')
 # print(full_time_employee.get_unpaid_vacation('2023-07-01', 5))
